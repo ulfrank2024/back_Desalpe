444bab66322a56e73be6d60e92c1079aa7bdee9d
ulfrank2024 <2724548@collegelacite.ca>
2025-08-27 02:58:01 -0400

    commit deployement render1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,37 +9,38 @@ app.use(cors());
 app.use(express.json());
 
 // Importation et utilisation des routes
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
-const countryRoutes = require('./routes/countryRoutes');
-app.use('/api/countries', countryRoutes);
+const countryRoutes = require("./routes/countryRoutes");
+app.use("/api/countries", countryRoutes);
 
-const cityRoutes = require('./routes/cityRoutes');
-app.use('/api/cities', cityRoutes);
+const cityRoutes = require("./routes/cityRoutes");
+app.use("/api/cities", cityRoutes);
 
-const sessionRoutes = require('./routes/sessionRoutes');
-app.use('/api/sessions', sessionRoutes);
+const sessionRoutes = require("./routes/sessionRoutes");
+app.use("/api/sessions", sessionRoutes);
 
-const anonymousClickRoutes = require('./routes/anonymousClickRoutes');
-app.use('/api/anonymous-clicks', anonymousClickRoutes);
+const anonymousClickRoutes = require("./routes/anonymousClickRoutes");
+app.use("/api/anonymous-clicks", anonymousClickRoutes);
 
-const invitationRoutes = require('./routes/invitationRoutes');
-app.use('/api/invitations', invitationRoutes);
+const invitationRoutes = require("./routes/invitationRoutes");
+app.use("/api/invitations", invitationRoutes);
 
-const registrationRoutes = require('./routes/registrationRoutes');
-app.use('/api/registrations', registrationRoutes);
+const registrationRoutes = require("./routes/registrationRoutes");
+app.use("/api/registrations", registrationRoutes);
 
-const emailRoutes = require('./routes/emailRoutes');
-app.use('/api/email', emailRoutes);
+// Correction de la casse : "Routes" devient "routes"
+const emailRoutes = require("./routes/emailRoutes");
+app.use("/api/email", emailRoutes);
 
 // NEW: Reminder Routes
-const reminderRoutes = require('./Routes/reminderRoutes');
-app.use('/api/reminders', reminderRoutes);
+// Correction de la casse : "Routes" devient "routes"
+const reminderRoutes = require("./routes/reminderRoutes");
+app.use("/api/reminders", reminderRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Serveur GraceDesalpe avec Supabase est en marche !');
+app.get("/", (req, res) => {
+    res.send("Serveur GraceDesalpe avec Supabase est en marche !");
 });
 
-app.listen(port, () => {
-});
+app.listen(port, () => {});
